925c52606e66dd17803fe19d48c7968d844bf0f3
usamasaeedantech <mudassir@saeedantechpvt.com>
2024-10-30 12:16:02 +0500

    Card changes

--- controller/cardController.js
+++ controller/cardController.js
@@ -1,34 +1,29 @@
 const Card = require('../models/card'); 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncError = require('../middlewares/catchAsyncError');
-const board = require('../models/board');
-const list = require('../models/list');
+const Board = require('../models/board');
+const List = require('../models/list');
 
 exports.createCard = catchAsyncError(async (req, res, next) => {
     const { name, boardId, listId } = req.body;
 
-    // Check if the board exists
-    const Board = await board.findById(boardId);
-    if (!Board) {
+    const board = await Board.findById(boardId);
+    if (!board) {
         return next(new ErrorHandler('Board not found', 404));
     }
 
-    // Check if the list exists and belongs to the board
-    const List = await list.findOne({ _id: listId, boardId: boardId });
-    if (!List) {
+    const list = await List.findOne({ _id: listId, boardId });
+    if (!list) {
         return next(new ErrorHandler('List not found or does not belong to this board', 404));
     }
 
-    // Create the card
     const card = await Card.create({
         name,
         boardId,
         listId,
     });
-
-    // Add the entire card object to the list's cards array
-    List.cards.push(card);
-    await List.save(); // Save the updated list with the new card
+    list.cards.push(card);
+    await list.save();
 
     res.status(201).json({
         success: true,
@@ -36,22 +31,16 @@ exports.createCard = catchAsyncError(async (req, res, next) => {
     });
 });
 
-exports.getAllCards = catchAsyncError(async (req, res, next) => {
-    const { listId } = req.query; 
-    const cards = await Card.find({ listId });
-
-    res.status(200).json({
-        success: true,
-        cards
-    });
-});
 
 exports.getCardById = catchAsyncError(async (req, res, next) => {
-    const card = await Card.findById(req.params.id);
+    const { id: cardId } = req.params;
 
-    if (!card) {
+    const list = await List.findOne({ "cards._id": cardId });
+
+    if (!list) {
         return next(new ErrorHandler('Card not found', 404));
     }
+    const card = list.cards.id(cardId);
 
     res.status(200).json({
         success: true,
@@ -59,19 +48,22 @@ exports.getCardById = catchAsyncError(async (req, res, next) => {
     });
 });
 
-// Update a card
 exports.updateCard = catchAsyncError(async (req, res, next) => {
-    let card = await Card.findById(req.params.id);
+    const { id: cardId } = req.params;
+    const { name } = req.body;
 
-    if (!card) {
+    // Find the list containing the card
+    const list = await List.findOne({ "cards._id": cardId });
+
+    if (!list) {
         return next(new ErrorHandler('Card not found', 404));
     }
 
-    card = await Card.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false
-    });
+    // Find the card in the list's cards array and update it
+    const card = list.cards.id(cardId);
+    card.name = name || card.name; // Update the fields as needed
+
+    await list.save();
 
     res.status(200).json({
         success: true,
@@ -79,15 +71,20 @@ exports.updateCard = catchAsyncError(async (req, res, next) => {
     });
 });
 
-// Delete a card
-exports.deleteCard = catchAsyncError(async (req, res, next) => {
-    const card = await Card.findById(req.params.id);
 
-    if (!card) {
+exports.deleteCard = catchAsyncError(async (req, res, next) => {
+    const { id: cardId } = req.params;
+
+    // Find the list containing the card and remove the card from the array
+    const list = await List.findOneAndUpdate(
+        { "cards._id": cardId },
+        { $pull: { cards: { _id: cardId } } },
+        { new: true }
+    );
+
+    if (!list) {
         return next(new ErrorHandler('Card not found', 404));
     }
-
-    await Card.deleteOne({ _id: req.params.id });
 
     res.status(200).json({
         success: true,
--- routes/card.js
+++ routes/card.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const { createCard, getAllCards, getCardById, updateCard, deleteCard } = require('../controller/cardController');
+const { createCard, getCardById, updateCard, deleteCard } = require('../controller/cardController');
 const { isAuthenticated } = require('../middlewares/auth');
 const router = express.Router();
 
 router.route('/card').post(isAuthenticated, createCard)
-router.route('/cards').get(isAuthenticated, getAllCards)
 router.route('/card/:id').get(isAuthenticated, getCardById)
 router.route('/card/:id').put(isAuthenticated, updateCard)
 router.route('/card/:id').delete(isAuthenticated, deleteCard)
