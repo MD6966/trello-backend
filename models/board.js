5ed55504bee394b60b2ecb873b37d15b2f432658
usamasaeedantech <mudassir@saeedantechpvt.com>
2024-11-12 15:35:19 +0500

    Reapply "File upload system created"
    This reverts commit ab13e67557090bc145672c19a9bb31edd6fc1534.

--- models/board.js
+++ models/board.js
@@ -1,25 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const boardScehma = mongoose.Schema({
+const boardSchema = mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please Enter Card Title'],
-    }, 
-    color_code : {
+    },
+    color_code: {
         type: String,
     },
-    notes : {
+    notes: {
         type: String,
     },
     status: {
-        type:Boolean,
-        default:false
+        type: Boolean,
+        default: false,
     },
-        createdAt: {
-            type: Date,
-            default: Date.now
+    files: [
+        {
+            url: String,
+            name: String,
         }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
-})
-
-module.exports = mongoose.model('Board', boardScehma)
+module.exports = mongoose.model('Board', boardSchema);
