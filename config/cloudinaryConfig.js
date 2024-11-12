2b84eb646f3bef341494725926c7ef9da6b5769a
usamasaeedantech <mudassir@saeedantechpvt.com>
2024-11-12 14:10:22 +0500

    File upload system created

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,10 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: "dn1hw0wsd",
+    api_key: "136859617267828",
+    api_secret: "NWwKwkteADn6FtzXnh9uOaqTW7g",
+});
+
+
+module.exports = cloudinary;
